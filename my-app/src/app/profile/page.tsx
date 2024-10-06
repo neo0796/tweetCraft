05005b30706cbea7c6b581455746b8e004a74ece
manjeet0796 <manjeetsharma0796@gmail.com>
2024-10-06 10:04:07 +0530

    claimAmt correct

--- my-app/src/app/profile/page.tsx
+++ my-app/src/app/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
+import axios from 'axios';
 
 const ProfilePageDC: React.FC = () => {
   const searchParams = useSearchParams();
@@ -13,6 +14,9 @@ const ProfilePageDC: React.FC = () => {
 
   const redeem = () => {
     setShowModal(true);
+    const clientAddress = account;
+    const claimAmt = balance?.replaceAll("TWC", '').trim();
+    axios.post('http://localhost:9000/withdraw', { clientAddress, claimAmt })
     setTimeout(() => setShowModal(false), 2000); // Hide modal after 2 seconds
   };
 
